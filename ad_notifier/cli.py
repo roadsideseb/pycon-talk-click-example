2b0a08054b8a7f11ea2f28c63b3d9ac104c460f1
Sebastian Vetter <seb@roadsi.de>
2016-05-21 18:27:36 -0700

    Add documentation and help text

--- ad_notifier/cli.py
+++ ad_notifier/cli.py
@@ -21,10 +21,14 @@ def process_url(url, email, reset_cache):
 
 @click.group()
 @click.argument('url')
-@click.option('--email')
-@click.option('--reset-cache', default=False, is_flag=True)
+@click.option('--email', help='email to send notifications to')
+@click.option('--reset-cache', default=False, is_flag=True,
+              help='reset the internal ads cache')
 @click.pass_context
 def main(context, url, email, reset_cache):
+    """
+    Check pinkbike ads for URL and (optionally) send email notification.
+    """
     context.obj = {'url': url,
                    'email': email,
                    'reset_cache': reset_cache}
@@ -33,13 +37,20 @@ def main(context, url, email, reset_cache):
 @main.command()
 @click.pass_context
 def run_once(context):
+    """
+    Run check for new ads once.
+    """
     process_url(**context.obj)
 
 
 @main.command()
-@click.option('--run-every', default=5, type=int)
+@click.option('--run-every', default=5, type=int,
+              help='run the ad check every X minutes')
 @click.pass_context
 def run_periodically(context, run_every):
+    """
+    Run scheduler to check for new ads periodically.
+    """
     print('Running as scheduled job, every {} minute(s)'.format(run_every))
 
     schedule.every(run_every).minutes.do(process_url, **context.obj)
